d859f8357e0333d942b46288973097bf4706a330
SALVADORPOETA <dsml@live.com.mx>
2023-05-05 05:03:11 -0600

    labels

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -35,12 +35,12 @@ const Signin = () => {
         </div>
         <form onSubmit={handleSubmit}>
             <div className='flex flex-col py-2'>
-                <label className='py-2 font-medium'>Email Address</label>
-                <input onChange={(e) => setEmail(e.target.value)} className='border p-3' type="email" />
+                <label for='email' className='py-2 font-medium'>Email Address</label>
+                <input id='email' onChange={(e) => setEmail(e.target.value)} className='border p-3' type="email" />
             </div>
             <div className='flex flex-col py-2'>
-                <label className='py-2 font-medium'>Password</label>
-                <input onChange={(e) => setPassword(e.target.value)} className='border p-3' type="password" />
+                <label for='password' className='py-2 font-medium'>Password</label>
+                <input id='password' onChange={(e) => setPassword(e.target.value)} className='border p-3' type="password" />
             </div>
             <button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white'>Sign In</button>
         </form>
